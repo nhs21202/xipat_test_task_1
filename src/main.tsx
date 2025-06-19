a49b34c38315467a1d01def93584db824c31da05
SonNH <nguyenhoason.job@gmail.com>
2025-06-19 19:29:10 +0700

    fix: remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
 import App from "./App";
+import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
